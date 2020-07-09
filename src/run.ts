f1c7be76538832812fcdc2a9a06a780edcf678c7
Simonas Daniliauskas <daniliauskas.s@gmail.com>
2020-07-09 22:44:58 +0300

    separate main form run

--- src/run.ts
+++ src/run.ts
@@ -2,94 +2,89 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import {parseIssueNumber} from './helpers/issueNumberParser'
 
-// import * as handler from './handler'
-
 export async function run(): Promise<void> {
   try {
-    const token = core.getInput('github-token')
-    const octokit = github.getOctokit(token)
-
-    process.stdout.write('auto pr issue label has started...')
+    const githubToken = core.getInput('github-token')
+    const octokit = github.getOctokit(githubToken)
 
     const context = github.context
-    if (context.issue.number === undefined) {
-      return
-    }
 
-    const inReviewLabel = 'In-Review'
-    const resolvedTestItLabel = 'Resolved (test it)'
+    if (context.issue.number !== undefined) {
+      const res = await octokit.issues.get({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.issue.number
+      })
 
-    const owner = context.repo.owner
-    const repo = context.repo.repo
-
-    const issue = await octokit.issues.get({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      issue_number: context.issue.number
-    })
-
-    const issueNumberFromBody = parseIssueNumber(owner, repo, issue.data.body)
-
-    process.stdout.write(issueNumberFromBody)
-
-    if (!Number(issueNumberFromBody)) {
-      return
-    }
-
-    //   const responseLabels = await octokit.issues.listLabelsForRepo({
-    //     owner: owner,
-    //     repo: repo
-    //   })
-
-    const isMerged = context.payload.pull_request && context.payload.pull_request['merged']
-
-    const issueLabelsResponse = await octokit.issues.listLabelsOnIssue({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      issue_number: Number(issueNumberFromBody)
-    })
-
-    if (isMerged) {
-      const issueLabel = issueLabelsResponse.data.find(
-        (l: {name: string}) => l.name === inReviewLabel
+      const issueNumberFromBody = parseIssueNumber(
+        context.repo.owner,
+        context.repo.repo,
+        res.data.body
       )
 
-      if (issueLabel !== undefined) {
-        await octokit.issues.removeLabel({
-          owner: owner,
-          repo: repo,
-          issue_number: Number(issueNumberFromBody),
-          name: inReviewLabel
+      process.stdout.write(context.repo.owner)
+      process.stdout.write(context.repo.repo)
+      process.stdout.write(res.data.body.toString())
+      process.stdout.write('issue number is' + issueNumberFromBody)
+      // process.stdout.write(res.data.title)
+      // process.stdout.write(res.data.body)
+
+      const responseLabels = await octokit.issues.listLabelsForRepo({
+        owner: context.repo.owner,
+        repo: context.repo.repo
+      })
+
+      // const isMerged =
+      //   context.payload.pull_request &&
+      //   context.payload.pull_request['merged'] === true
+
+      process.stdout.write('before check')
+      if (Number(issueNumberFromBody)) {
+        // add in review label
+        const inReviewLabel = 'In-Review'
+        const issueLabelsResponse = await octokit.issues.listLabelsOnIssue({
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+          issue_number: Number(issueNumberFromBody)
         })
-      }
+        const issueLabel = issueLabelsResponse.data.find(l => l.name === inReviewLabel)
+        if (issueLabel !== undefined) {
+          await octokit.issues.removeLabel({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: Number(issueNumberFromBody),
+            name: inReviewLabel
+          })
+        }
 
-      const resolvedTestIt = issueLabelsResponse.data.find(
-        (l: {name: string}) => l.name === resolvedTestItLabel
-      )
+        // add resolved label
+        process.stdout.write('inside issue')
+        const resolvedTestItLabel = 'Resolved (test it)'
+        const resolvedTestIt = responseLabels.data.find(l => l.name === resolvedTestItLabel)
 
-      if (resolvedTestIt === undefined) {
+        process.stdout.write('before creating checking')
+        if (resolvedTestIt === undefined) {
+          await octokit.issues.createLabel({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            name: resolvedTestItLabel,
+            description: 'resolved and test it',
+            color: '#000000'
+          })
+        }
+        process.stdout.write('before adding label')
         await octokit.issues.addLabels({
-          owner: owner,
-          repo: repo,
+          owner: context.repo.owner,
+          repo: context.repo.repo,
           issue_number: Number(issueNumberFromBody),
           labels: [resolvedTestItLabel]
         })
-      }
-    } else {
-      const issueLabel = issueLabelsResponse.data.find(
-        (l: {name: string}) => l.name === inReviewLabel
-      )
-      if (issueLabel === undefined) {
-        await octokit.issues.addLabels({
-          owner: owner,
-          repo: repo,
-          issue_number: Number(issueNumberFromBody),
-          labels: [inReviewLabel]
-        })
+        return
+      } else {
+        // issue not found
+        return
       }
     }
-
-    // await handler.handle(octokit, github.context)
   } catch (error) {
     core.setFailed(error.message)
   }
