75a47a4d652b434cdc5333b70201187b49097107
Simonas Daniliauskas <daniliauskas.s@gmail.com>
2020-07-09 22:22:51 +0300

    add additional debug check

--- src/run.ts
+++ src/run.ts
@@ -7,6 +7,8 @@ export async function run(): Promise<void> {
     const token = core.getInput('github-token')
     const octokit = github.getOctokit(token)
 
+    process.stdout.write('auto pr issue label has started...')
+
     await handler.handle(octokit, github.context)
   } catch (error) {
     core.setFailed(error.message)
