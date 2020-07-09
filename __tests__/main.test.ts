f0697f9e5fad9918f461e53fb7bacaa2a0fb8421
Simonas Daniliauskas <daniliauskas.s@gmail.com>
2020-07-09 21:21:54 +0300

    add workaround for eslint errors

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,18 +1,4 @@
-import {wait} from '../src/wait'
 import {parseIssueNumber} from '../src/helpers/issueNumberParser'
-
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
 
 test('test github issue parsing', async () => {
   const description =
